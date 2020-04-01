77b9ffbc9769d2ab43a35aaa509c046e64399add
Zhestovskih Vyacheslav <dogmevich@gmail.com>
2020-04-01 20:31:38 +0300

    'initial'

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+  #app(@click="getClicks") {{ message }}
+</template>
+
+<script>
+import { mapMutations, mapState } from "vuex";
+
+export default {
+  name: 'App',
+
+  computed: {
+    ...mapState(['count']),
+    message() {
+      return this.count > 0 ? `You clicked ${this.count} times` : 'Click me';
+    }
+  },
+
+  methods: {
+    ...mapMutations(['increment']),
+    getClicks() {
+      this['increment']();
+    }
+  },
+}
+</script>
+
+<style lang="stylus">
+#app 
+  display flex
+  height 100%
+  justify-content center
+  align-items center
+  cursor pointer
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,152 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const OptimizeCssAssestPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = !isDev
+
+const optimization = () => {
+  const config = {
+    splitChunks: {
+      chunks: 'all'
+    }
+  }
+
+  if (isProd) {
+    config.minimizer = [
+      new OptimizeCssAssestPlugin(),
+      new TerserWebpackPlugin()
+    ]
+  }
+
+  return config;
+}
+
+const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`;
+
+const vueStyleLoaders = extra => {
+  const loaders = [
+    isDev ? 'vue-style-loader' :  MiniCssExtractPlugin.loader,
+    'css-loader'
+  ]
+
+  if (extra) {
+    loaders.push(extra)
+  }
+
+  return loaders
+}
+
+const babelOptions = preset => {
+  const opts = {
+    presets: [
+      '@babel/preset-env'
+    ],
+    plugins: [
+      '@babel/plugin-proposal-class-properties'
+    ]
+  }
+
+  if (preset) {
+    opts.presets.push(preset);
+  }
+
+  return opts;
+}
+
+const jsLoaders = () => {
+  const loaders = [{
+    loader: 'babel-loader',
+    options: babelOptions()
+  }]
+
+  if (isDev) {
+    loaders.push('eslint-loader')
+  }
+
+  return loaders
+}
+
+const plugins = () => {
+  const base = [
+    new HTMLWebpackPlugin({
+      template: 'index.html'
+    }),
+    new VueLoaderPlugin(),
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin([
+      {
+        from: path.resolve(__dirname, 'src/favicon.ico'),
+        to: path.resolve(__dirname, 'dist')
+      }
+    ]),
+    new MiniCssExtractPlugin({
+      filename: 'app.[hash].css'
+    })
+  ]
+
+  return base;
+}
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: {
+    main: ['@babel/polyfill', './index.js']
+  },
+  resolve: {
+    extensions: ['.js', '.vue'],
+    alias: {
+      '@': path.resolve(__dirname),
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  },
+  optimization: optimization(),
+  devServer: {
+    port: 4200,
+    hot: isDev
+  },
+  devtool: isDev ? 'source-map' : '',
+  output: {
+    filename: filename('js'),
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: plugins(),
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          extractCSS: true
+        }
+      },
+      {
+        test: /\.pug$/,
+        loader: 'pug-plain-loader'
+      },
+      {
+        test: /\.styl(us)?$/,
+        use: vueStyleLoaders('stylus-loader')
+      },
+      {
+        test: /\.(png|img|jpg|svg|gif)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(ttf|wof|woff|eot)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: jsLoaders()
+      },
+    ]
+  }
+}
